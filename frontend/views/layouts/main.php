aca1dc7abbf54531671d945f258febd5b2ab587c
Marcellus Barrus <mbarrus@rllinsure.com>
2018-01-29 17:51:45 -0700

    no message

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -51,7 +51,7 @@ AppAsset::register($this);
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
+                'Logout',
                 ['class' => 'btn btn-link logout']
             )
             . Html::endForm()
