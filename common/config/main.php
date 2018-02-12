590d6d74551349b655fe4f4ec60bad96fc50e4bb
Marcellus Barrus <mbarrus73@gmail.com>
2018-02-11 17:40:06 -0700

    -Added the social widget. -Added the fontawsome asset

--- common/config/main.php
+++ common/config/main.php
@@ -5,6 +5,41 @@ return [
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+	'modules' => [
+		'social' => [
+			// the module class
+			'class' => 'kartik\social\Module',
+			
+			// the global settings for the disqus widget
+			'disqus' => [
+				'settings' => ['shortname' => 'DISQUS_SHORTNAME'] // default settings
+			],
+			
+			// the global settings for the facebook plugins widget
+			'facebook' => [
+				'appId' => '1339131572857692',
+				'secret' => '1ca598a7c8a3aa829370934875285406',
+			],
+			
+			// the global settings for the google plugins widget
+			'google' => [
+				'clientId' => 'GOOGLE_API_CLIENT_ID',
+				'pageId' => 'GOOGLE_PLUS_PAGE_ID',
+				'profileId' => 'GOOGLE_PLUS_PROFILE_ID',
+			],
+			
+			// the global settings for the google analytic plugin widget
+			'googleAnalytics' => [
+				'id' => 'TRACKING_ID',
+				'domain' => 'TRACKING_DOMAIN',
+			],
+			
+			// the global settings for the twitter plugin widget
+			'twitter' => [
+				'screenName' => 'TWITTER_SCREEN_NAME'
+			],
+		],
+	],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
