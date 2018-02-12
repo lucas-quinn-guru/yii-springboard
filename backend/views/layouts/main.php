e8d9dc30319cb8ae7499ac93ab553ff5de88d72e
Marcellus Barrus <mbarrus73@gmail.com>
2018-02-12 07:00:18 -0700

    -Added navigation to the different controllers.

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -4,13 +4,16 @@
 /* @var $content string */
 
 use backend\assets\AppAsset;
+use backend\assets\FontAwesomeAsset;
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
+use common\helpers\PermissionHelpers;
 
-AppAsset::register($this);
+AppAsset::register( $this );
+FontAwesomeAsset::register( $this );
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -20,7 +23,7 @@ AppAsset::register($this);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
+    <title><?= Html::encode( $this->title ) ?></title>
     <?php $this->head() ?>
 </head>
 <body>
@@ -28,32 +31,62 @@ AppAsset::register($this);
 
 <div class="wrap">
     <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
+
+    $is_admin = false;
+
+    if (!Yii::$app->user->isGuest) {
+	    $is_admin = PermissionHelpers::requireMinimumRole( "Admin" );
+
+
+	    NavBar::begin( [
+		    'brandLabel' => Yii::$app->name . " <i class=\"fa fa-plug\"></i> Admin ",
+		    'brandUrl' => Yii::$app->homeUrl,
+		    'options' => [
+			    'class' => 'navbar-inverse navbar-fixed-top',
+		    ],
+	    ] );
+    } else
+    {
+	    NavBar::begin( [
+		    'brandLabel' => Yii::$app->name . " <i class=\"fa fa-plug\"></i>",
+		    'brandUrl' => Yii::$app->homeUrl,
+		    'options' => [
+			    'class' => 'navbar-inverse navbar-fixed-top',
+		    ],
+	    ] );
+
+	    $menuItems = [
+		    ['label' => 'Home', 'url' => ['/site/index']],
+	    ];
     }
+
+    if( !Yii::$app->user->isGuest && $is_admin )
+    {
+	    $menuItems[] = [ 'label' => 'Users', 'url' => [ 'user/index' ] ];
+	    $menuItems[] = [ 'label' => 'Profiles', 'url' => [ 'profile/index' ] ];
+	    $menuItems[] = [ 'label' => 'Roles', 'url' => [ 'role/index' ] ];
+	    $menuItems[] = [ 'label' => 'User Types', 'url' => [ 'user-type/index' ] ];
+	    $menuItems[] = [ 'label' => 'Statuses', 'url' => [ 'status/index' ] ];
+    }
+
+
+    if( Yii::$app->user->isGuest )
+    {
+        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    } else
+    {
+	    $menuItems[] = [
+		    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+		    'url' => ['/site/logout'],
+		    'linkOptions' => ['data-method' => 'post']
+	    ];
+    }
+
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => $menuItems,
     ]);
+
     NavBar::end();
     ?>
 
